5b754fb60440271b5489fc71f4e89f02011dcde6
Hugues Valois <hugues_valois@hotmail.com>
2026-08-07 13:16:27 -0700

    Workaround threading issue in consolonia controls gallery app.

--- src/Woohoo.ConsoloniaControls.Gallery/Program.cs
+++ src/Woohoo.ConsoloniaControls.Gallery/Program.cs
@@ -11,6 +11,21 @@ internal class Program
     [STAThread]
     private static void Main(string[] args)
     {
+        // Bug in Consolonia? Without this force initialization of Dispatcher.UIThread,
+        // the UIThread is initialized in MTA and is a background thread.
+        var dispatcher = Avalonia.Threading.Dispatcher.UIThread;
+
+        TaskScheduler.UnobservedTaskException += (sender, eventArgs) =>
+        {
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+
+            ThreadPool.QueueUserWorkItem(state =>
+                throw new InvalidOperationException("An unobserved task exception occurred.", eventArgs.Exception));
+        };
+
         BuildAvaloniaApp()
             .StartWithConsoleLifetime(args);
     }
